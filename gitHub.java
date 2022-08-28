fd761af0ab1c87ef9725707635cbea2f861f7bb4
vallavanram <vallavanshanmugaraj@gmail.com>
2022-08-28 10:33:05 +0530

    login

---
+++ gitHub.java
@@ -0,0 +1,22 @@
+package GitHub;
+
+public class gitHub {
+	
+	private void vallavan() {
+		System.out.println("100");
+	}
+	private void ram() {
+		System.out.println("100");
+		
+
+	}
+	public static void main(String[] args) {
+		gitHub vr = new gitHub();
+		vr.vallavan();
+		vr.ram();
+	}
+	
+	
+	
+
+}
